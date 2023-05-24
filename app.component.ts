7fbe063d01d9bfbc6760c079c1a60db65b46e6ad
Trk@ <94073172+truptikhade@users.noreply.github.com>
2023-05-25 04:05:35 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Register Form';
+  displayname='';
+  displayaddress='';
+  displaycontact='';
+  displayemail='';
+  getValue(name:string,address:string,contact:string,email:string){
+    this.displayname=name;
+    this.displayaddress=address;
+    this.displaycontact=contact;
+    this.displayemail=email;
+  }
+}
